37be40c1e0fea21face5035c9c3ee5c2ed2951ed
Zach <sierrazja@gmail.com>
2024-12-12 22:13:21 -0600

    final changes

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,9 +1,6 @@
-
-function injectCss(){
-    const header = document.getElementsByTagName('head')[0];
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = '../scripts/NSA.css';
-    header.appendChild(link);
-}
+const header = document.getElementsByTagName('head')[0];
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = '../scripts/NSA.css';
+header.appendChild(link);
